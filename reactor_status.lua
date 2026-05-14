67c24304c52e9bb9447ed7af745b03cc3f56285b
temporar23 <chronsystems@gmail.com>
2026-05-14 17:23:34 +0200

    Create reactor_status.lua

---
+++ reactor_status.lua
@@ -0,0 +1,32 @@
+local monitor = peripheral.find("monitor")
+
+rednet.open("back")
+
+monitor.setTextScale(1)
+
+while true do
+    local id, msg = rednet.receive()
+
+    if msg.type == "reactor" then
+
+        monitor.clear()
+        monitor.setCursorPos(1,1)
+
+        monitor.write("=== REACTOR ===")
+
+        monitor.setCursorPos(1,3)
+        monitor.write("STATUS: " .. msg.status)
+
+        monitor.setCursorPos(1,5)
+        monitor.write("TEMP: " .. msg.temp .. " C")
+
+        monitor.setCursorPos(1,7)
+        monitor.write("COOLANT: " .. msg.coolant .. "%")
+
+        monitor.setCursorPos(1,9)
+        monitor.write("DAMAGE: " .. msg.damage .. "%")
+
+        monitor.setCursorPos(1,11)
+        monitor.write("BURN: " .. msg.burn)
+    end
+end
